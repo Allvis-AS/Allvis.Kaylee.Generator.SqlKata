71672eee7d34c77515ccefa79a51641a78353615
Sindre van der Linden <graknol@gmail.com>
2020-11-17 14:15:58 +0100

    QueryFactoryExtensions generates.

---
+++ Allvis.Kaylee.Generator.SqlKata.Tests/Unit/Writers/QueryFactoryExtensionsWriterTest.cs
@@ -0,0 +1,101 @@
+using Xunit;
+using Xunit.Categories;
+using Allvis.Kaylee.Generator.SqlKata.Writers;
+using System.IO;
+using Allvis.Kaylee.Generator.SqlKata.Tests.Fixtures;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Allvis.Kaylee.Generator.SqlKata.Utilities;
+using Allvis.Kaylee.Analyzer;
+
+namespace Allvis.Kaylee.Generator.SqlKata.Tests.Unit.Extensions
+{
+    [UnitTest]
+    public class QueryFactoryExtensionsWriterTest
+    {
+        [Fact]
+        public async Task TestWrite()
+        {
+            // Arrange
+            var tSchema = AuthSchemaFixture.Create();
+            var ast = KayleeHelper.Parse(tSchema);
+            // Act
+            var source = QueryFactoryExtensionsWriter.Write(ast);
+            // Assert
+            await DebugUtils.WriteGeneratedFileToDisk("QueryFactoryExtensions.cs", source).ConfigureAwait(false);
+            Assert.Equal(@"namespace Allvis.Kaylee.Generated.SqlKata.Extensions
+{
+    public static class QueryFactoryExtensions
+    {
+        public static async System.Threading.Tasks.Task<bool> Exists_auth_User(this SqlKata.Execution.QueryFactory _db, System.Guid userId)
+        {
+            var _rows = await _db.GetAsync<int>(Queries.Exists_auth_User(userId)).ConfigureAwait(false);
+            return System.Linq.Enumerable.Any(_rows);
+        }
+        public static System.Threading.Tasks.Task<Models.auth.User> Get_auth_User(this SqlKata.Execution.QueryFactory _db, System.Guid userId)
+        {
+            return _db.FirstAsync<Models.auth.User>(Queries.Get_auth_User(userId));
+        }
+        public static System.Threading.Tasks.Task<System.Collections.Generic.IEnumerable<Models.auth.User>> Get_auth_User(this SqlKata.Execution.QueryFactory _db)
+        {
+            return _db.GetAsync<Models.auth.User>(Queries.Get_auth_User());
+        }
+        public static System.Threading.Tasks.Task<int> Insert_auth_User(this SqlKata.Execution.QueryFactory _db, System.Guid? userId, string? firstName, string? lastName, string contactEmail)
+        {
+            return _db.ExecuteAsync(Queries.Insert_auth_User(userId, firstName, lastName, contactEmail));
+        }
+        public static System.Threading.Tasks.Task<int> Insert_auth_User(this SqlKata.Execution.QueryFactory _db, System.Collections.Generic.IEnumerable<(System.Guid UserId, string? FirstName, string? LastName, string ContactEmail)> rows)
+        {
+            return _db.ExecuteAsync(Queries.Insert_auth_User(rows));
+        }
+        public static System.Threading.Tasks.Task<int> Delete_auth_User(this SqlKata.Execution.QueryFactory _db, System.Guid userId)
+        {
+            return _db.ExecuteAsync(Queries.Delete_auth_User(userId));
+        }
+        public static System.Threading.Tasks.Task<int> Update_auth_User_FullName(this SqlKata.Execution.QueryFactory _db, System.Guid userId, string? firstName, string? lastName)
+        {
+            return _db.ExecuteAsync(Queries.Update_auth_User_FullName(userId, firstName, lastName));
+        }
+        public static System.Threading.Tasks.Task<int> Update_auth_User_ContactInformation(this SqlKata.Execution.QueryFactory _db, System.Guid userId, string contactEmail)
+        {
+            return _db.ExecuteAsync(Queries.Update_auth_User_ContactInformation(userId, contactEmail));
+        }
+        public static async System.Threading.Tasks.Task<bool> Exists_auth_UserRole(this SqlKata.Execution.QueryFactory _db, System.Guid userId, System.Guid roleId)
+        {
+            var _rows = await _db.GetAsync<int>(Queries.Exists_auth_UserRole(userId, roleId)).ConfigureAwait(false);
+            return System.Linq.Enumerable.Any(_rows);
+        }
+        public static System.Threading.Tasks.Task<Models.auth.UserRole> Get_auth_UserRole(this SqlKata.Execution.QueryFactory _db, System.Guid userId, System.Guid roleId)
+        {
+            return _db.FirstAsync<Models.auth.UserRole>(Queries.Get_auth_UserRole(userId, roleId));
+        }
+        public static System.Threading.Tasks.Task<System.Collections.Generic.IEnumerable<Models.auth.UserRole>> Get_auth_UserRole(this SqlKata.Execution.QueryFactory _db, System.Guid userId)
+        {
+            return _db.GetAsync<Models.auth.UserRole>(Queries.Get_auth_UserRole(userId));
+        }
+        public static System.Threading.Tasks.Task<System.Collections.Generic.IEnumerable<Models.auth.UserRole>> Get_auth_UserRole(this SqlKata.Execution.QueryFactory _db)
+        {
+            return _db.GetAsync<Models.auth.UserRole>(Queries.Get_auth_UserRole());
+        }
+        public static System.Threading.Tasks.Task<int> Insert_auth_UserRole(this SqlKata.Execution.QueryFactory _db, System.Guid userId, System.Guid roleId, int? flag)
+        {
+            return _db.ExecuteAsync(Queries.Insert_auth_UserRole(userId, roleId, flag));
+        }
+        public static System.Threading.Tasks.Task<int> Insert_auth_UserRole(this SqlKata.Execution.QueryFactory _db, System.Collections.Generic.IEnumerable<(System.Guid UserId, System.Guid RoleId, int Flag)> rows)
+        {
+            return _db.ExecuteAsync(Queries.Insert_auth_UserRole(rows));
+        }
+        public static System.Threading.Tasks.Task<int> Delete_auth_UserRole(this SqlKata.Execution.QueryFactory _db, System.Guid userId, System.Guid roleId)
+        {
+            return _db.ExecuteAsync(Queries.Delete_auth_UserRole(userId, roleId));
+        }
+        public static System.Threading.Tasks.Task<int> Update_auth_UserRole_Flag(this SqlKata.Execution.QueryFactory _db, System.Guid userId, System.Guid roleId, int flag)
+        {
+            return _db.ExecuteAsync(Queries.Update_auth_UserRole_Flag(userId, roleId, flag));
+        }
+    }
+}
+", source);
+        }
+    }
+}
---
+++ Allvis.Kaylee.Generator.SqlKata/Writers/QueryFactoryExtensionsWriter.cs
@@ -0,0 +1,194 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Allvis.Kaylee.Analyzer.Models;
+using Allvis.Kaylee.Generator.SqlKata.Builders;
+using Allvis.Kaylee.Generator.SqlKata.Extensions;
+using CaseExtensions;
+
+namespace Allvis.Kaylee.Generator.SqlKata.Writers
+{
+    public static class QueryFactoryExtensionsWriter
+    {
+        public static string Write(Ast ast)
+        {
+            var sb = new SourceBuilder();
+            sb.PublicStaticClass("Allvis.Kaylee.Generated.SqlKata.Extensions", "QueryFactoryExtensions", sb =>
+            {
+                foreach (var schema in ast.Schemata)
+                {
+                    sb.Write(schema);
+                }
+            });
+            return sb.ToString();
+        }
+
+        private static void Write(this SourceBuilder sb, Schema schema)
+        {
+            foreach (var entity in schema.Entities)
+            {
+                sb.Write(entity);
+            }
+        }
+
+        private static void Write(this SourceBuilder sb, Entity entity)
+        {
+            sb.WriteExists(entity);
+            sb.WriteGet(entity);
+            sb.WriteInsert(entity);
+            sb.WriteInsertMany(entity);
+            sb.WriteDelete(entity);
+            foreach (var mutation in entity.Mutations)
+            {
+                sb.WriteUpdate(mutation);
+            }
+            foreach (var child in entity.Children)
+            {
+                sb.Write(child);
+            }
+        }
+
+        private static void WriteExists(this SourceBuilder sb, Entity entity)
+        {
+            var entityName = entity.DisplayName.Replace(".", "").Replace("::", "_");
+            var fullPrimaryKey = entity.GetFullPrimaryKey();
+            var parameters = fullPrimaryKey.Select(fr =>
+            {
+                var field = fr.ResolvedField;
+                return (Type: field.Type.ToCSharp(), Name: field.Name.ToCamelCase());
+            });
+            sb.PublicStaticMethod("async System.Threading.Tasks.Task<bool>", $"Exists_{entityName}", parameters.PrefixWithQueryFactory(), sb =>
+            {
+                var arguments = string.Join(", ", parameters.Select(p => p.Name));
+                sb.AL($"var _rows = await _db.GetAsync<int>(Queries.Exists_{entityName}({arguments})).ConfigureAwait(false);");
+                sb.AL("return System.Linq.Enumerable.Any(_rows);");
+            });
+        }
+
+        private static void WriteGet(this SourceBuilder sb, Entity entity)
+        {
+            var modelName = $"Models.{entity.DisplayName.Replace(".", "").Replace("::", ".")}";
+            var entityName = entity.DisplayName.Replace(".", "").Replace("::", "_");
+            var fullPrimaryKey = entity.GetFullPrimaryKey().ToList();
+            var parameters = fullPrimaryKey.Select(fr =>
+            {
+                var field = fr.ResolvedField;
+                return (field.Type.ToCSharp(), field.Name.ToCamelCase());
+            }).ToList();
+            var allFields = fullPrimaryKey.Select(fr => fr.ResolvedField).Concat(entity.Fields).Distinct().ToList();
+
+            var stackedFullPrimaryKey = new Stack<FieldReference>(fullPrimaryKey);
+            var stackedParameters = new Stack<(string Type, string Name)>(parameters);
+
+            var i = stackedParameters.Count;
+            while (i >= 0)
+            {
+                var singular = stackedParameters.Count == parameters.Count;
+                var returnType = singular
+                    ? $"System.Threading.Tasks.Task<{modelName}>"
+                    : $"System.Threading.Tasks.Task<System.Collections.Generic.IEnumerable<{modelName}>>";
+                sb.PublicStaticMethod(returnType, $"Get_{entityName}", stackedParameters.Reverse().PrefixWithQueryFactory(), sb =>
+                {
+                    var getMethod = singular ? "FirstAsync" : "GetAsync";
+                    var arguments = string.Join(", ", stackedParameters.Reverse().Select(p => p.Name));
+                    sb.AL($@"return _db.{getMethod}<{modelName}>(Queries.Get_{entityName}({arguments}));");
+                });
+                if (stackedParameters.Count > 0)
+                {
+                    stackedFullPrimaryKey.Pop();
+                    stackedParameters.Pop();
+                }
+                i--;
+            }
+        }
+
+        private static void WriteInsert(this SourceBuilder sb, Entity entity)
+        {
+            bool IsOptional(Field field)
+            {
+                var partOfParentKey = field.Entity != entity;
+                return !partOfParentKey && (field.HasDefault() || field.Nullable);
+            }
+
+            var entityName = entity.DisplayName.Replace(".", "").Replace("::", "_");
+            var fullPrimaryKey = entity.GetFullPrimaryKey();
+            var allFields = fullPrimaryKey.Select(fr => fr.ResolvedField).Concat(entity.Fields).Distinct();
+            var parameters = allFields.Select(f => (Optional: IsOptional(f), Type: f.Type.ToCSharp(), Name: f.Name.ToCamelCase()));
+            sb.PublicStaticMethod("System.Threading.Tasks.Task<int>", $"Insert_{entityName}", parameters.PrefixWithQueryFactory(), sb =>
+            {
+                var arguments = string.Join(", ", parameters.Select(p => p.Name));
+                sb.AL($"return _db.ExecuteAsync(Queries.Insert_{entityName}({arguments}));");
+            });
+        }
+
+        private static void WriteInsertMany(this SourceBuilder sb, Entity entity)
+        {
+            bool IsNullable(Field field)
+            {
+                var partOfParentKey = field.Entity != entity;
+                return !partOfParentKey && field.Nullable;
+            }
+
+            var entityName = entity.DisplayName.Replace(".", "").Replace("::", "_");
+            var fullPrimaryKey = entity.GetFullPrimaryKey();
+            var allFields = fullPrimaryKey.Select(fr => fr.ResolvedField).Concat(entity.Fields).Distinct();
+            var tupleParameters = allFields.Select(f =>
+            {
+                var nullable = IsNullable(f);
+                var type = f.Type.ToCSharp();
+                return (nullable ? $"{type}?" : type, f.Name.ToPascalCase());
+            });
+            var parameters = new[] { (Type: $"System.Collections.Generic.IEnumerable<({tupleParameters.Join()})>", Name: "rows") };
+            sb.PublicStaticMethod("System.Threading.Tasks.Task<int>", $"Insert_{entityName}", parameters.PrefixWithQueryFactory(), sb =>
+            {
+                var arguments = string.Join(", ", parameters.Select(p => p.Name));
+                sb.AL($"return _db.ExecuteAsync(Queries.Insert_{entityName}({arguments}));");
+            });
+        }
+
+        private static void WriteDelete(this SourceBuilder sb, Entity entity)
+        {
+            var entityName = entity.DisplayName.Replace(".", "").Replace("::", "_");
+            var fullPrimaryKey = entity.GetFullPrimaryKey();
+            var parameters = fullPrimaryKey.Select(fr =>
+            {
+                var field = fr.ResolvedField;
+                return (Type: field.Type.ToCSharp(), Name: field.Name.ToCamelCase());
+            });
+            sb.PublicStaticMethod("System.Threading.Tasks.Task<int>", $"Delete_{entityName}", parameters.PrefixWithQueryFactory(), sb =>
+            {
+                var arguments = string.Join(", ", parameters.Select(p => p.Name));
+                sb.AL($"return _db.ExecuteAsync(Queries.Delete_{entityName}({arguments}));");
+            });
+        }
+
+        private static void WriteUpdate(this SourceBuilder sb, Mutation mutation)
+        {
+            bool IsNullable(Field field)
+            {
+                var partOfParentKey = field.Entity != mutation.Entity;
+                return !partOfParentKey && field.Nullable;
+            }
+
+            var entityName = mutation.Entity.DisplayName.Replace(".", "").Replace("::", "_");
+            var mutationName = mutation.Name;
+            var fullPrimaryKey = mutation.Entity.GetFullPrimaryKey();
+            var allFields = fullPrimaryKey.Concat(mutation.FieldReferences).Select(fr => fr.ResolvedField).Distinct();
+            var parameters = allFields.Select(f =>
+            {
+                return (Optional: IsNullable(f), Type: f.Type.ToCSharp(), Name: f.Name.ToCamelCase());
+            });
+            sb.PublicStaticMethod("System.Threading.Tasks.Task<int>", $"Update_{entityName}_{mutationName}", parameters.PrefixWithQueryFactory(), sb =>
+            {
+                var arguments = string.Join(", ", parameters.Select(p => p.Name));
+                sb.AL($"return _db.ExecuteAsync(Queries.Update_{entityName}_{mutationName}({arguments}));");
+            });
+        }
+
+        private static IEnumerable<(bool Optional, string Type, string Name)> PrefixWithQueryFactory(this IEnumerable<(bool Optional, string Type, string Name)> parameters)
+            => new[] { (false, "this SqlKata.Execution.QueryFactory", "_db") }.Concat(parameters);
+
+        private static IEnumerable<(string Type, string Name)> PrefixWithQueryFactory(this IEnumerable<(string Type, string Name)> parameters)
+            => new[] { ("this SqlKata.Execution.QueryFactory", "_db") }.Concat(parameters);
+    }
+}
